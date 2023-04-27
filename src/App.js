162dc8e0c2f4735d51e0ffdc6b1a0536068a7eda
NikKowPHP <nik.kow@outlook.com>
2023-04-27 08:47:32 +0200

    add tasks component to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { Header } from './components/Header';
+import { Tasks } from './components/Tasks';
 class App extends Component {
   render() {
     return (
       <div className='container'>
         <Header  />
+			  <Tasks  />
       </div>
     );
   }
