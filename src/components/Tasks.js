897cf4ca0214548de694a8bc26457b693b44363b
NikKowPHP <nik.kow@outlook.com>
2023-04-28 07:39:10 +0200

    use usestate for Tasks

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,10 +1,8 @@
-import React from 'react'
-import{ useState } from 'react'
+import React, { useState } from 'react'
 
 
-
-export const Tasks = () => {
-	const [task, setTasks] = useState(
+ export const Tasks = () => {
+	const [tasks, setTasks] = useState([
 	{
 		id:1,
 		text: 'doctors',
@@ -22,14 +20,15 @@ export const Tasks = () => {
 		text: 'food shopping',
 		day: '10.05.15',
 		reminder: true,
-	})
+	}
+
+	])
+
 	return (
 		<div>
 
 			{tasks.map((task) => 
-			(<h3 key={task.id}> {task.text}</h3>
-			
-			))}
+			(<h3 key={task.id}> {task.text}</h3>))}
 
 		</div>
 	)
