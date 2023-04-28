499de47d3a2274c05b6082df3c40df1eb05f4ea7
NikKowPHP <nik.kow@outlook.com>
2023-04-28 08:20:14 +0200

    move state to App

--- src/App.js
+++ src/App.js
@@ -1,36 +1,38 @@
-import{ useState } from 'react'
-import React, { Component } from 'react';
-import { Header } from './components/Header';
-import  { Tasks }  from './components/Tasks';
+import { useState } from "react";
+import React, { Component } from "react";
+import { Header } from "./components/Header";
+import { Tasks } from "./components/Tasks";
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [
+        {
+          id: 1,
+          text: "doctors",
+          day: "10.05.15",
+          reminder: true,
+        },
+        {
+          id: 2,
+          text: "meeting",
+          day: "10.05.15",
+          reminder: true,
+        },
+        {
+          id: 3,
+          text: "food shopping",
+          day: "10.05.15",
+          reminder: true,
+        },
+      ],
+    };
+  }
   render() {
-
-	const [tasks, setTasks] = useState([
-	{
-		id:1,
-		text: 'doctors',
-		day: '10.05.15',
-		reminder: true,
-	},
-	{
-		id:2,
-		text: 'meeting',
-		day: '10.05.15',
-		reminder: true,
-	},
-	{
-		id:3,
-		text: 'food shopping',
-		day: '10.05.15',
-		reminder: true,
-	}
-
-	])
-
     return (
-      <div className='container'>
-        <Header  />
-			  <Tasks tasks={tasks} />
+      <div className="container">
+        <Header />
+        <Tasks tasks={this.state.tasks} />
       </div>
     );
   }
