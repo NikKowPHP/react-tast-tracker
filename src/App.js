9da03d61ad7d8d025189b257cdaaef8da623e0d6
NikKowPHP <nik.kow@outlook.com>
2023-04-28 07:40:17 +0200

    import tasks within app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import{ useState } from 'react'
 import React, { Component } from 'react';
 import { Header } from './components/Header';
-import { Tasks } from './components/Tasks';
+import  { Tasks }  from './components/Tasks';
 class App extends Component {
   render() {
+
     return (
       <div className='container'>
         <Header  />
